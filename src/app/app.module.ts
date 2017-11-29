d1389cd0d5f1c702582236051e97442d615db89e
Akash <akashspotdar@github.com>
2017-11-29 15:20:34 +0530

    Gallery component created and selector set in navbar.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,15 @@ import { AlertModule } from 'ngx-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NavbarComponent } from './navbar/navbar.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ImageDetailComponent } from './image-detail/image-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    GalleryComponent,
+    ImageDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/gallery/gallery.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery',
+  templateUrl: './gallery.component.html',
+  styleUrls: ['./gallery.component.css']
+})
+export class GalleryComponent implements OnInit {
+
+  photos: string = 'Recent Photos';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
