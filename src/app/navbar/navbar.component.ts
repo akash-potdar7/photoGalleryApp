5f96a626eae64a125048b1ccb61f6185dec03057
Akash <akashspotdar@github.com>
2017-11-29 14:48:32 +0530

    Navbar in place, installed bootstrap ngx. Also installed Google fonts: Open sans.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  title: string = "Navbar";
+  title: string = "Gallery";
 
   constructor() { }
 
